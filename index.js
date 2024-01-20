b86f79f8648aa8643f1564e95f7ef0784b032d0c
Saúl Omar Hernández Gallegos <saul.omar55@gmail.com>
2024-01-21 00:52:36 +0100

    Refactor API endpoints and add example request object

--- index.js
+++ index.js
@@ -18,31 +18,55 @@ app.listen(port, () => {
   console.log(`🌏 Server is running at http://localhost:${port}`);
 });
 
-app.post("/random_joke", (_, res) => {
-  const reqUrl = encodeURI("http://official-joke-api.appspot.com/random_joke");
-  http.get(
-    reqUrl,
-    (resp) => {
-      let data = "";
-
-      // A chunk of data has been recieved.
-      resp.on("data", (chunk) => {
-        data += chunk;
-      });
-
-      // The whole response has been received.
-      resp.on("end", () => {
-        res.status(200).send(JSON.parse(data));
-      });
+let exampleReq = {
+  responseId: "response-id",
+  session: "projects/project-id/agent/sessions/session-id",
+  queryResult: {
+    queryText: "End-user expression",
+    parameters: {
+      "param-name": "param-value",
     },
-    (err) => {
-      console.log("Error: ", err);
-    }
-  );
-});
+    allRequiredParamsPresent: true,
+    fulfillmentText: "Response configured for matched intent",
+    fulfillmentMessages: [
+      {
+        text: {
+          text: ["Response configured for matched intent"],
+        },
+      },
+    ],
+    outputContexts: [
+      {
+        name: "projects/project-id/agent/sessions/session-id/contexts/context-name",
+        lifespanCount: 5,
+        parameters: {
+          "param-name": "param-value",
+        },
+      },
+    ],
+    intent: {
+      name: "projects/project-id/agent/intents/intent-id",
+      displayName: "matched-intent-name",
+    },
+    intentDetectionConfidence: 1,
+    diagnosticInfo: {},
+    languageCode: "en",
+  },
+  originalDetectIntentRequest: {},
+};
 
-app.post("/random_activity", (_, res) => {
-  const reqUrl = encodeURI("http://www.boredapi.com/api/activity/");
+app.post("/random", (req, res) => {
+  let type = exammpleReq.intent.displayName;
+  let reqUrl = "";
+
+  if (type == "tellme a joke") {
+    reqUrl = encodeURI("http://official-joke-api.appspot.com/random_joke");
+  } else if (type == "give me an activity") {
+    reqUrl = encodeURI("http://www.boredapi.com/api/activity/");
+  } else {
+    res.status(400).send("Invalid type");
+  }
+
   http.get(
     reqUrl,
     (resp) => {
