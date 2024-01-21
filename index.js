064aca6f47cce96b76d5e794afe69c4ed518315a
Saúl Omar Hernández Gallegos <saul.omar55@gmail.com>
2024-01-21 17:01:04 +0100

    Fix typo in variable name and add webhook response
    This commit fixes a typo in the variable name `exammpleReq` and replaces it with `req`. Additionally, it adds a webhook response to the `/random` endpoint.

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const express = require("express");
-// will use this later to send requests
-const http = require("http");
-// import env variables
-require("dotenv").config();
+import express from "express";
+import fetch from "node-fetch";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,72 +15,25 @@ app.listen(port, () => {
   console.log(`🌏 Server is running at http://localhost:${port}`);
 });
 
-let exampleReq = {
-  responseId: "response-id",
-  session: "projects/project-id/agent/sessions/session-id",
-  queryResult: {
-    queryText: "End-user expression",
-    parameters: {
-      "param-name": "param-value",
-    },
-    allRequiredParamsPresent: true,
-    fulfillmentText: "Response configured for matched intent",
-    fulfillmentMessages: [
-      {
-        text: {
-          text: ["Response configured for matched intent"],
-        },
-      },
-    ],
-    outputContexts: [
-      {
-        name: "projects/project-id/agent/sessions/session-id/contexts/context-name",
-        lifespanCount: 5,
-        parameters: {
-          "param-name": "param-value",
-        },
-      },
-    ],
-    intent: {
-      name: "projects/project-id/agent/intents/intent-id",
-      displayName: "matched-intent-name",
-    },
-    intentDetectionConfidence: 1,
-    diagnosticInfo: {},
-    languageCode: "en",
-  },
-  originalDetectIntentRequest: {},
-};
-
-app.post("/random", (req, res) => {
-  let type = exammpleReq.intent.displayName;
+app.post("/", (req, res) => {
+  let type = req.body.queryResult.intent.displayName;
   let reqUrl = "";
 
-  if (type == "tellme a joke") {
-    reqUrl = encodeURI("http://official-joke-api.appspot.com/random_joke");
+  if (type == "tell me a joke") {
+    reqUrl = "http://official-joke-api.appspot.com/random_joke";
   } else if (type == "give me an activity") {
-    reqUrl = encodeURI("http://www.boredapi.com/api/activity/");
+    reqUrl = "http://www.boredapi.com/api/activity/";
   } else {
     res.status(400).send("Invalid type");
   }
 
-  http.get(
-    reqUrl,
-    (resp) => {
-      let data = "";
-
-      // A chunk of data has been recieved.
-      resp.on("data", (chunk) => {
-        data += chunk;
-      });
-
-      // The whole response has been received.
-      resp.on("end", () => {
-        res.status(200).send(JSON.parse(data));
-      });
-    },
-    (err) => {
-      console.log("Error: ", err);
-    }
-  );
+  fetch(reqUrl, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      res.status(200).send(json);
+    })
+    .catch((error) => res.status(400).send("Error" + error));
 });
